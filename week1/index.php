bce80a69b349189cce4bc0dd1f4988af51bff24b
Dirham Putra <92032977+EEuRekaa@users.noreply.github.com>
2024-09-10 23:04:58 +0700

    update
    ouw

---
+++ week1/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Week 1</title>
+</head>
+
+<body>
+    <div>
+        <h1>Minggu 1</h1>
+    </div>
+    <div>
+        <p>
+            Halo dunia <br> Selamat datang di <br>Halaman pertama <br>HTML
+        </p>
+    </div>
+</body>
+
+</html>
